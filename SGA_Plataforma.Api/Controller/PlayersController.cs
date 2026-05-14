6da9e6899bc417bc89976946fb3284a9dcaaefcc
WillianChiquinato <willianchiquinato@hotmail.com>
2026-05-14 19:43:47 -0300

    Fix: player to use GetPlayerByUserId

--- SGA_Plataforma.Api/Controller/PlayersController.cs
+++ SGA_Plataforma.Api/Controller/PlayersController.cs
@@ -7,5 +7,19 @@ namespace SGA_Plataforma.Api.Controller;
 [Route("api/[controller]")]
 public sealed class PlayersController : CrudControllerBase<Player>
 {
-    public PlayersController(ICrudService<Player> service) : base(service) { }
+    private readonly IPlayerService _playerService;
+
+    public PlayersController(IPlayerService playerService) : base(playerService)
+    {
+        _playerService = playerService;
+    }
+
+    [HttpGet]
+    [Route("GetPlayerByUserId")]
+    public async Task<IActionResult> GetPlayerByUserId([FromQuery] int userId)
+    {
+        var playerUser = await _playerService.GetUserPlayerByIdAsync(userId);
+
+        return playerUser.Result != null ? Ok(playerUser) : BadRequest(playerUser);
+    }
 }
